5766133287eaeabc58b04ad46c207255fc491b13
Mahima Parekh <mahimaparekh98@gmail.com>
2025-05-14 16:14:42 +0530

    Added some js code

---
+++ src/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".frame1").addEventListener("click", function(){
+    document.querySelector(".frame1").style.display = "none";
+    document.querySelector(".frame2").style.display = "block";
+})
